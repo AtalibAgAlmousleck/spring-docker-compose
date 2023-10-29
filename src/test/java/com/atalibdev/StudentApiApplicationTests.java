9e7de06a5a58970fa7d307213b7233228688545f
AtalibAgAlmousleck <atalibletouareg@gmail.com>
2023-10-29 20:07:26 +0800

    Adding the index file

--- src/test/java/com/atalibdev/StudentApiApplicationTests.java
+++ src/test/java/com/atalibdev/StudentApiApplicationTests.java
@@ -1,13 +1,4 @@
 package com.atalibdev;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class StudentApiApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
